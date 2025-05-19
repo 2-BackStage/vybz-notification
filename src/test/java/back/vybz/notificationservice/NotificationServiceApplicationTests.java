92ea4d3438d6eb5e1bc69040219d80fdd3e6c0d3
jin-sung-oh <wlsqoren@naver.com>
2025-05-19 15:04:39 +0900

    init: create notificationservice

---
+++ src/test/java/back/vybz/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package back.vybz.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
